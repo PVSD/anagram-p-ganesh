be72dd3d8c0a84951785543aa137e91a88c5eb06
p-ganesh <pg304@pvsd.org>
2018-09-17 11:22:36 -0400

    be nice to frogs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbreader = new Scanner(System.in);
+
+        System.out.println("Enter your first word.");
+        String word1 = kbreader.next();
+
+        System.out.println("Enter your second word.");
+        String word2 = kbreader.next();
+
+        if (word1.length() != word2.length()) {
+            System.out.println("Sorry, your words are not anagrams.");
+
+        }
+
+        else if (word1.length() == word2.length()) {
+            int character;
+            for (character = 0; character <= word1.length(); character++) {
+                int a = word1.substring(character, character++).length();
+
+            }
+        }
     }
 }
